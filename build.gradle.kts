7826841127476de9a68918e70c9a126a0d52c83f
HTVC579 <vinh.htv@gmail.com>
2026-08-19 20:37:20 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.cloudstream)
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle") version "master-SNAPSHOT"
 }
 
 android {
@@ -11,9 +11,17 @@ android {
     defaultConfig {
         minSdk = 21
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 cloudstream {
     repoUrl = "https://github.com/HTVC579/BL"
 }
- 
